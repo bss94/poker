1a65c0842b0db2852245c6a2db99a72aaa191d68
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-05-18 00:56:40 +0600

    end lvl 1: create component Card.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-
-import './App.css'
+import "./App.css";
+import Card from "./components/Card.tsx";
 
 function App() {
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <div className="playingCards faceImages">
+            <Card rank = 'k' suit='diams'/>
+            <Card rank = 'q' suit='spades'/>
+            <Card rank = '2' suit='hearts'/>
+            <Card rank = 'j' suit='clubs'/>
+        </div>
+    );
 }
-
-export default App
+export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+interface Props {
+    rank: string;
+    suit: string;
+}
+
+interface Suit {
+    key: string;
+    symbol: string;
+}
+
+const Card: React.FC<Props> = ({rank, suit}) => {
+    const suitCase: Suit[] = [
+        {key: "diams", symbol: "♦"},
+        {key: "hearts", symbol: "♥"},
+        {key: "clubs", symbol: "♣"},
+        {key: "spades", symbol: "♠"}
+    ];
+
+    return (
+        <span className={`card rank-${rank} ${suit}`}>
+            <span className="rank">{rank.toUpperCase()}</span>
+            <span className="suit">
+                {suitCase.map((el:Suit) => {
+                    if(el.key===suit){
+                        return el.symbol
+                    }
+                })}
+            </span>
+        </span>
+    );
+};
+
+export default Card;
