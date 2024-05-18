0b140e8c3241c403156be6800ba7e82859b1243e
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-05-19 01:33:35 +0600

    complete lvl 3: create function start, step, reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import './App.css';
 import Card from './components/Card/Card.tsx';
 import CardDeck from './lib/CardDeck.ts';
+import {useState} from 'react';
+import card from './lib/Card.ts';
 
 
 function App() {
-    const cardDeck = new CardDeck();
+    const [state, setState] = useState<card[]>([]);
+    const [deck, setDeck] = useState<CardDeck>();
+    console.log(state);
 
-    const hand = cardDeck.getCards(5);
-    console.log(cardDeck);
+
+    const start = () => {
+        const cards = new CardDeck();
+        setState(cards.getCards(5));
+        setDeck(cards);
+    };
+    const step = () => {
+        if (deck !== undefined) {
+            if (deck.deck.length >= 5) {
+                setState(deck.getCards(5));
+            } else {
+                setState(deck.getCards(deck.deck.length));
+            }
+        }
+    };
+    const reset = () => {
+        setDeck(undefined);
+        setState([]);
+    };
+
     return (
-        <div className="playingCards faceImages">
-            {/*<Card rank={cardDeck.deck[1].rank} suit={cardDeck.deck[1].suit}/>*/}
-            {/*<Card rank={ccc.rank} suit={ccc.suit}/>*/}
-            {/*<Card rank="a" suit="hearts"/>*/}
-            {/*<Card rank="j" suit="clubs"/>*/}
-            {/*<Card rank="10" suit="clubs"/>*/}
-            {hand.map((el) => {
-                return <Card rank={el.rank} suit={el.suit} key={el.rank + el.suit}/>;
-            })}
-        </div>
+        <>
+            {!deck ? <button onClick={start}>start game</button> :
+                <>
+                    {deck ? <p>{deck.deck.length}</p> : <p>0</p>}
+                    {deck.deck.length === 0 ? <button onClick={reset}>reset</button> :
+                        <button onClick={step}>get cards</button>}
+                    <div className="playingCards faceImages">
+                        {state.map((el) => {
+                            return <Card rank={el.rank} suit={el.suit} key={el.rank + el.suit}/>;
+                        })}
+                    </div>
+                </>
+
+            }
+        </>
+
     );
 }
 
